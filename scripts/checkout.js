2ddbd624039085671c2f36442e9ac2b749d37396
Praveen Shukla <praveenshukla2575@gmail.com>
2026-07-23 20:02:56 +0530

    Created an OOP version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import { renderOrdersummary } from "./checkout/ordersummary.js";
 import { renderpaymentsummary } from "./checkout/paymentsummary.js";
+import '../data/cart-oop.js';
+
 
 renderOrdersummary();
 renderpaymentsummary();
