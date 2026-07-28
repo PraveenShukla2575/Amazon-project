da0630611eb64eeba57b53b456ba77a3548b83d1
Praveen Shukla <praveenshukla2575@gmail.com>
2026-07-28 22:44:58 +0530

    Added Backend Practice File

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrdersummary } from "./checkout/ordersummary.js";
 import { renderpaymentsummary } from "./checkout/paymentsummary.js";
-
+import '../data/backend-practice.js';
 
 renderOrdersummary();
 renderpaymentsummary();
