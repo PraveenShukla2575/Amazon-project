ef8392f055dfc1e8e12d4bf3add217549da3d335
Praveen Shukla <praveenshukla2575@gmail.com>
2026-07-29 23:04:16 +0530

    Load products in beforeAll hook

--- data/products.js
+++ data/products.js
@@ -793,9 +793,9 @@ export function loadProducts(fun)
       }
       return new Product(productDetails);
     });
-    console.log('load products');
-
-    fun();
+    if (typeof fun === 'function') {
+      fun();
+    }
   });
 
   xhr.open('GET','https://supersimplebackend.dev/products');
